66f7a40be9ee55b68c4413c1727a60efdbd2babf
Shashank Siripragada <siripragada.s@northeastern.edu>
2021-09-25 02:44:37 -0400

    Adding Assignment1 Files

---
+++ src/model/Person.java
@@ -0,0 +1,214 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author sirip
+ */
+public class Person {
+    private String name;
+    private String address;
+    private String dob;
+    private String phonePrimary;
+    private String phoneSecondary;
+    private String emailPrimary;
+    private String emailSecondary;
+    private String fax;
+    private String ssn;
+    private String mrn;
+    private String healthPlanNumber;
+    private String bankPrimary;
+    private String bankSecondary;
+    private String licenseNumber;
+    private String licensePlate;
+    private String device;
+    private String linkedin;
+    private String ipaddr;   
+    public ImageIcon personImage;
+    public ImageIcon relatedImage;
+    public ImageIcon biometric;
+    private String unique;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhonePrimary() {
+        return phonePrimary;
+    }
+
+    public void setPhonePrimary(String phonePrimary) {
+        this.phonePrimary = phonePrimary;
+    }
+
+    public String getPhoneSecondary() {
+        return phoneSecondary;
+    }
+
+    public void setPhoneSecondary(String phoneSecondary) {
+        this.phoneSecondary = phoneSecondary;
+    }
+
+    public String getEmailPrimary() {
+        return emailPrimary;
+    }
+
+    public void setEmailPrimary(String emailPrimary) {
+        this.emailPrimary = emailPrimary;
+    }
+
+    public String getEmailSecondary() {
+        return emailSecondary;
+    }
+
+    public void setEmailSecondary(String emailSecondary) {
+        this.emailSecondary = emailSecondary;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMrn() {
+        return mrn;
+    }
+
+    public void setMrn(String mrn) {
+        this.mrn = mrn;
+    }
+
+    public String getHealthPlanNumber() {
+        return healthPlanNumber;
+    }
+
+    public void setHealthPlanNumber(String healthPlanNumber) {
+        this.healthPlanNumber = healthPlanNumber;
+    }
+
+    public String getBankPrimary() {
+        return bankPrimary;
+    }
+
+    public void setBankPrimary(String bankPrimary) {
+        this.bankPrimary = bankPrimary;
+    }
+
+    public String getBankSecondary() {
+        return bankSecondary;
+    }
+
+    public void setBankSecondary(String bankSecondary) {
+        this.bankSecondary = bankSecondary;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIpaddr() {
+        return ipaddr;
+    }
+
+    public void setIpaddr(String ipaddr) {
+        this.ipaddr = ipaddr;
+    }
+
+    public ImageIcon getPersonImage() {
+        return personImage;
+    }
+
+    public void setPersonImage(ImageIcon personImage) {
+        this.personImage = personImage;
+    }
+
+    public ImageIcon getRelatedImage() {
+        return relatedImage;
+    }
+
+    public void setRelatedImage(ImageIcon relatedImage) {
+        this.relatedImage = relatedImage;
+    }
+
+    public ImageIcon getBiometric() {
+        return biometric;
+    }
+
+    public void setBiometric(ImageIcon biometric) {
+        this.biometric = biometric;
+    }
+
+    public String getUnique() {
+        return unique;
+    }
+
+    public void setUnique(String unique) {
+        this.unique = unique;
+    }
+    
+}
